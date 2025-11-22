7b3f997317199dafb7206f0bb0a1efcd66339577
Oliverngu <138084319+Oliverngu@users.noreply.github.com>
2025-11-22 19:14:11 +0100

    Update config.ts

--- src/core/firebase/config.ts
+++ src/core/firebase/config.ts
@@ -7,11 +7,14 @@ const firebaseConfig = {
   apiKey: "AIzaSyCB7ZTAhDlRwueGW6jqDdMqmpfHOI62mtE",
   authDomain: "mintleaf-74d27.firebaseapp.com",
   projectId: "mintleaf-74d27",
-  storageBucket: "mintleaf-74d27.appspot.com",
+  storageBucket: "mintleaf-74d27.firebasestorage.app",
   messagingSenderId: "1053273095803",
   appId: "1:1053273095803:web:84670303a5324c0d816cde",
   measurementId: "G-2Y86CZ0633"
 };
+
+const bucket = "gs://mintleaf-74d27.firebasestorage.app"; 
+// ha a console tetején gs://mintleaf-74d27.firebasestorage.app van, akkor AZT írd ide
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
@@ -19,3 +22,4 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export { Timestamp, serverTimestamp };
+export const storage = getStorage(app, "gs://mintleaf-74d27.firebasestorage.app");
